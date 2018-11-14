14d01cd68310854cd39e7a9c8a4d9627a5f845e4
HSBoyWPR <332107301@qq.com>
2018-11-14 21:09:33 +0800

    添加没用的读取storage代码

--- js/background.js
+++ js/background.js
@@ -1,3 +1,18 @@
 /**
  * Created by 丕汝 on 2018/11/13.
  */
+
+function sendMessageToContentScript(message, callback) {
+    //这里的url填写要操作的页面,我是要操作的微信文章页
+    chrome.tabs.query({url:"*://*.baidu.com/*"}, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+            if (callback) callback(response);
+        });
+    });
+};
+
+function sendMsg(data) {
+    sendMessageToContentScript(data, function (resp) {
+        console.log('来自content的回复：' + resp);
+    })
+}
--- js/inter_index.js
+++ js/inter_index.js
@@ -2,12 +2,31 @@
  * Created by 丕汝 on 2018/11/13.
  */
 $(function () {
+    //监听消息
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        console.log(request);
+        //chrome.storage.sync.set({"key": keyWords}, function () {
+        alert('保存成功');
+// });
 
+        sendResponse('接收成功');
+    });
+
+
+    let keyWords = ['千图网', '千库网'];
+    chrome.storage.sync.get(['key'], function (data) {
+        if (data['key']) {
+            keyWords = data['key'];
+        }
+        console.log("加载关键字数量：" + keyWords.length);
+
+    });
     console.log("关键字拦截服务启动成功!");
     let func = function () {
         console.log('interceptStation 扫描网页中...');
         initServer();
     };
+
     //监听全局ajax完成事件
     $('input[type=submit]').click(function () {
         let a = false;
@@ -19,9 +38,6 @@ $(function () {
     initServer();
 
     function initServer() {
-        let keyWords = ['千图网', '千库网'];
-      //  keyWords=chrome.storage.sync.get({"intercept_keyword":['千图网', '千库网']});
-
         let ls = $('.t');
         for (let i of ls) {
             let title = i.innerText;
